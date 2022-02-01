6738154ff2fe01fa14a22cbf8d05fd010370a2f9
courtsideKB <kbankhead1992.yahoo.com>
2022-02-01 02:46:47 -0800

    Server is able to make a get reqrequest in order to receive the signup.html page

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const got = require("got");
+import express from "express";
+import bodyParser from "body-parser";
+import got from "got";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
